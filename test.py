3ab41b75b3d20636283e20e47134cb4b720cf854
Adam Izdebski <42173829+AdamIzdebski@users.noreply.github.com>
2024-05-31 11:53:01 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-""" Test GPU availability.
+""" Test file that initializes a ddp run and counts the number of available GPUs.
 """
 
 import os, sys
@@ -9,7 +9,7 @@ import torch
 logger = logging.getLogger(__name__)
 logging.basicConfig(
     level=logging.INFO,
-    stream=sys.stdout,  # log outputs to the default file used by SLURM
+    stream=sys.stdout,
     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
     datefmt='%H:%M:%S',
 )
@@ -17,14 +17,11 @@ logging.basicConfig(
 
 def main():
 
-    logger.info("Torch test run:")
     logger.info(f"PyTorch version: {torch.__version__}")
     logger.info(f"CUDA available: {torch.cuda.is_available()}")
-    logger.info(f"CUDA version: {torch.version.cuda}")
-    logger.info(f"CUDNN version: {torch.backends.cudnn.version()}")
-    logger.info(f"Number of GPUs: {torch.cuda.device_count()}")
+    logger.info(f"Number of recognized GPUs: {torch.cuda.device_count()}")
     for i in range(torch.cuda.device_count()):
-        logger.info(torch.cuda.get_device_properties(i).name)
+        logger.info(f'CUDA:{i}: {torch.cuda.get_device_properties(i).name}')
 
 if __name__ == "__main__":
     main()
